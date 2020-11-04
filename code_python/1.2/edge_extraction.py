a36ed67051baf927ac99fed41f482c6e028186fc
Ricardo <r>
2020-11-04 14:57:35 +0000

    ddepth = cv2.CV_64F update

--- code_python/1.2/edge_extraction.py
+++ code_python/1.2/edge_extraction.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 
 def initilizeArs():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--type', type=str, default="scharr",
+    parser.add_argument('--type', type=str, default="sobel",
                         help='choose one:|sobel|scharr|prewitt|roberts|canny|laplacian|')
     parser.add_argument('--kernel_size', default=3,type=int, help='Sobel and Laplacian, canny Kernel Size int')
     parser.add_argument('--lower', default=100,type=int, help='Canny low threshold int')
@@ -17,7 +17,7 @@ def initilizeArs():
 
 def sobel(src,kernelSize):
     window_name = f"Gradient Operators to edge extraction\nSobel KernelSize={kernelSize}"
-    ddepth = -1
+    ddepth = cv2.CV_64F
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
     grad_x = cv2.Sobel(gray,ddepth,1,0,ksize=kernelSize)
     grad_y = cv2.Sobel(gray,ddepth,0,1,ksize=kernelSize)
@@ -26,11 +26,12 @@ def sobel(src,kernelSize):
     abs_grad_y = cv2.convertScaleAbs(grad_y)
     # [blend] Total Gradient (approximate)
     grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+
     return[abs_grad_x,abs_grad_y,grad,window_name]
 
 def scharr(src):
     window_name = f"Gradient Operators to edge extraction\nScharr"
-    ddepth = -1
+    ddepth = cv2.CV_64F
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
     grad_x = cv2.Scharr(gray,ddepth,1,0)
     grad_y = cv2.Scharr(gray,ddepth,0,1)
@@ -44,23 +45,34 @@ def scharr(src):
 def prewitt(src):
     window_name = f"Gradiprewittent Operators to edge extraction\nPrewitt"
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    ddepth = cv2.CV_64F
     kernelx = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
     kernely = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
-    #ddepth=-1, the output image will have the same depth as the source.
-    img_prewittx = cv2.filter2D(gray,-1, kernelx)
-    img_prewitty = cv2.filter2D(gray,-1, kernely)
-    return[img_prewittx,img_prewitty,img_prewittx + img_prewitty,window_name]
+    grad_x = cv2.filter2D(gray,ddepth, kernelx)
+    grad_y = cv2.filter2D(gray,ddepth, kernely)
+    ## [convert] converting back to uint8
+    abs_grad_x = cv2.convertScaleAbs(grad_x)
+    abs_grad_y = cv2.convertScaleAbs(grad_y)
+    ## [blend] Total Gradient (approximate)
+    grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+
+    return[abs_grad_x,abs_grad_y,grad,window_name]
     
 def roberts(src):
     window_name = f"Gradiprewittent Operators to edge extraction\nRoberts"
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    ddepth = cv2.CV_64F
     kernelx = np.array([[-1,0],[0,1]])
     kernely = np.array([[0,-1],[1,0]])
-    #ddepth=-1, the output image will have the same depth as the source.
-    img_Robertsx = cv2.filter2D(gray, -1, kernelx)
-    img_Robertsy = cv2.filter2D(gray, -1, kernely)
+    grad_x = cv2.filter2D(gray,ddepth, kernelx)
+    grad_y = cv2.filter2D(gray,ddepth, kernely)
+    ## [convert] converting back to uint8
+    abs_grad_x = cv2.convertScaleAbs(grad_x)
+    abs_grad_y = cv2.convertScaleAbs(grad_y)
+    ## [blend] Total Gradient (approximate)
+    grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
 
-    return[img_Robertsx,img_Robertsy,img_Robertsx + img_Robertsy,window_name]
+    return[abs_grad_x,abs_grad_y,grad,window_name]
 
 def canny(src,lower,upper,kernelSize):
     window_name = f"Gradient Operators to edge extraction\nCanny aperturesize={kernelSize}\n lower={lower} | upper={upper} "
@@ -70,7 +82,7 @@ def canny(src,lower,upper,kernelSize):
 
 def Laplacian(src,kernelSize):
     window_name = f"Gradient Operators to edge extraction\nLaplace KernelSize={kernelSize} "
-    ddepth = -1
+    ddepth = cv2.CV_64F
     src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
     dst = cv2.Laplacian(src_gray, ddepth, ksize=kernelSize)
     # [convert]# converting back to uint8
@@ -123,7 +135,7 @@ def run():
             plt.show()
             
     except:
-        prinr("IMG-Ploting error")
+        print("IMG-Ploting error")
 
 
 if __name__ == '__main__':
